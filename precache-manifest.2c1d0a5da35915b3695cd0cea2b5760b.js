60fabda6ecf27901b795e34dfc0b1c5d675e2a60
jayhun0521 <jayhun21@gmail.com>
2022-11-04 13:20:48 -0400

    Updates

---
+++ precache-manifest.2c1d0a5da35915b3695cd0cea2b5760b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d55b8e8935ee1a60e93",
+    "url": "/todoApp/static/css/main.c3bc48fb.chunk.css"
+  },
+  {
+    "revision": "3d55b8e8935ee1a60e93",
+    "url": "/todoApp/static/js/main.3d55b8e8.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/todoApp/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "1b4a16ba3f2171f1c785",
+    "url": "/todoApp/static/js/2.1b4a16ba.chunk.js"
+  },
+  {
+    "revision": "eb9e755f4a971b6f8529",
+    "url": "/todoApp/static/js/runtime~main.eb9e755f.js"
+  },
+  {
+    "revision": "5fd0ca6d0ef9cb0b26252452575e9f67",
+    "url": "/todoApp/index.html"
+  }
+];
